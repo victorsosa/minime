8560722b22e5783f9f9246e2fd8baedab062e320
victorsosa <victor.sosa@peopleware.do>
2019-04-28 07:43:22 -0400

    removed not used code and fix validations

--- app/controllers/links_controller.rb
+++ app/controllers/links_controller.rb
@@ -1,17 +1,15 @@
 # frozen_string_literal: true
 
 class LinksController < ApplicationController
-  before_action :set_link, only: %i[show edit update destroy]
-
   skip_before_action :verify_authenticity_token
 
   def go
-    @link = Link.find_by_in_url!(params[:in_url])
+    @link = Link.find_by!(in_url: params[:in_url])
     @link.counter += 1 # increse counter
     @link.save
     redirect_to @link.out_url, status: @link.http_status
-  rescue ActiveRecord::RecordNotFound => e
-    render html: @link.errors, status: :unprocessable_entity
+  rescue ActiveRecord::RecordNotFound
+    render json: @link.errors, status: :unprocessable_entity
   end
 
   def top
@@ -21,85 +19,11 @@ class LinksController < ApplicationController
 
   def generate_short_url
     # generate shorter url
-    if !params[:url].blank?
+    @link = Link.new(out_url: params[:url], counter: 1)
+    if @link.save
       render json: short_url(params[:url]), status: :ok
     else
       render json: @link.errors, status: :unprocessable_entity
     end
-  end
-
-  # GET /links
-  # GET /links.json
-  def index
-    @links = Link.all
-  end
-
-  # GET /links/1
-  # GET /links/1.json
-  def show; end
-
-  # GET /links/new
-  def new
-    @link = Link.new
-  end
-
-  # GET /links/1/edit
-  def edit; end
-
-  # POST /links
-  # POST /links.json
-  def create
-    @link = Link.new(link_params)
-
-    respond_to do |format|
-      if @link.save
-        format.html { redirect_to @link, notice: 'Link was successfully created.' }
-        format.json { render :show, status: :created, location: @link }
-      else
-        format.html { render :new }
-        format.json { render json: @link.errors, status: :unprocessable_entity }
-      end
-    end
-  end
-
-  # PATCH/PUT /links/1
-  # PATCH/PUT /links/1.json
-  def update
-    respond_to do |format|
-      if @link.update(link_params)
-        format.html { redirect_to @link, notice: 'Link was successfully updated.' }
-        format.json { render :show, status: :ok, location: @link }
-      else
-        format.html { render :edit }
-        format.json { render json: @link.errors, status: :unprocessable_entity }
-      end
-    end
-  end
-
-  # DELETE /links/1
-  # DELETE /links/1.json
-  def destroy
-    @link.destroy
-    respond_to do |format|
-      format.html { redirect_to links_url, notice: 'Link was successfully destroyed.' }
-      format.json { head :no_content }
-    end
-  end
-
-  private
-
-  def short_url(_url)
-    # generate shorter url
-    @link = Link.create!(out_url: _url, counter: 1)
-  end
-
-  # Use callbacks to share common setup or constraints between actions.
-  def set_link
-    @link = Link.find(params[:id])
-  end
-
-  # Never trust parameters from the scary internet, only allow the white list through.
-  def link_params
-    params.require(:link).permit(:in_url, :out_url, :http_status, :counter, :title)
   end
 end
